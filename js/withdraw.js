88a4b94f0580dd4baf0bb9fb1aed91711ace0f1b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-29 03:14:32 +0600

    withdraw and balance calculate with function

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,10 @@
-console.log('hello from withdraw');
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const newWithdrawAmount = getInputFieldValueById('withdraw-field');
+    const previousWithdrawAmount = getElementValueById('withdraw-element');
+    const newWithdrawTotal = newWithdrawAmount + previousWithdrawAmount;
+    setTextElementValueById('withdraw-element', newWithdrawTotal);
+
+    const previousBalanceTotal = getElementValueById('balance');
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById('balance', newBalanceTotal);
+})
