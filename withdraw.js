51d4bf5be91640d4630d2fc3ad2864d40fc0ebc6
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-29 01:13:11 +0600

    withdraw code organize done

--- withdraw.js
+++ withdraw.js
@@ -1,27 +1,20 @@
 document.getElementById("withdraw-btn").addEventListener("click", function () {
   const withdrawField = document.getElementById("withdraw-field");
   let newWithdrawAmountString = withdrawField.value;
-  const newWithdrawAmount = parseInt(newWithdrawAmountString);
+  const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
   const withdrawElement = document.getElementById("withdraw-element");
-  const withdrawElementString = withdrawElement.innerText;
-
-  /* const withdrawTotal = newWithdrawAmount + parseInt(withdrawElementString);
-
-  withdrawElement.innerText = withdrawTotal; */
+  const previousWithdrawString = withdrawElement.innerText;
+  const previousWithdraw = parseFloat(previousWithdrawString);
 
   const previousBalanceElement = document.getElementById("balance");
   const previousBalanceString = previousBalanceElement.innerText;
-  const previousBalance = parseInt(previousBalanceString);
+  const previousBalance = parseFloat(previousBalanceString);
 
   if (previousBalance > newWithdrawAmount) {
-    
-    const withdrawTotal = newWithdrawAmount + parseInt(withdrawElementString);
-
+    const withdrawTotal = newWithdrawAmount + previousWithdraw;
     withdrawElement.innerText = withdrawTotal;
-
     const updateBalance = previousBalance - newWithdrawAmount;
-
     previousBalanceElement.innerText = updateBalance;
   } else {
     alert("Your balance is not sufficient for withdraw.");
